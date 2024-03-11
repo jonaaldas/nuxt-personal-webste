fee9429836504e46e0ffb4749ec5924ddff5bc82
jonaaldas <jonaaldas@gmail.com>
2024-03-10 20:12:56 -0400

    fixed hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="container">
+	<div class="container flex-row flex">
 		<div class="w-2/4">
 			<h1 class="text-xl lg:text-2xl">Hey!</h1>
 			<p class="text-lg lg:text-xl">
@@ -9,6 +9,27 @@
 				. My expertise lies in creating dynamic, responsive web applications that prioritize user experience and performance. Through my projects, I've honed my skills in front-end
 				development, particularly with Vue.js and Nuxt.js, allowing me to craft beautiful applications.
 			</p>
+		</div>
+		<div class="w-2/4 mt-auto flex flex-row justify-end gap-5">
+			<a href="https://www.x.com/aldasdev" class="cursor-pointer" target="_blank" title="X">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 fill-white">
+					<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+				</svg>
+			</a>
+			<a href="mailto:j@aldas.dev" class="cursor-pointer" target="_blank" title="Email">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 fill-white">
+					<path
+						d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+					/>
+				</svg>
+			</a>
+			<a href="" class="cursor-pointer" target="_blank" title="Resume">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8 fill-white">
+					<path
+						d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"
+					/>
+				</svg>
+			</a>
 		</div>
 	</div>
 </template>
