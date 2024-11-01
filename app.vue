0c8331096f9b7c2de147974a97007c86d6212dbc
Jonathan Aldas <jonaaldas@gmail.com>
2024-11-01 20:00:48 +0100

    construction

--- app.vue
+++ app.vue
@@ -1,9 +1,13 @@
 <template>
-	<div>
-		<NuxtLayout>
-			<NavBar />
-			<NuxtPage />
-			<Footer />
-		</NuxtLayout>
-	</div>
+  <NuxtLayout>
+    <!-- <NavBar />
+    <NuxtPage />
+    <Footer /> -->
+    <div class="h-screen w-full flex justify-center items-center flex-col">
+      <h1 class="text-xl sm:text-2xl">Page under construction...</h1>
+      <p>
+        <a href="https://www.x.com/aldasdev">🚀 Aldas</a>
+      </p>
+    </div>
+  </NuxtLayout>
 </template>
